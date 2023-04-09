7c12908529dde4640ffd2585870ed00953729161
Christopher Leung <chrisleung360@yahoo.com>
2023-04-09 01:07:08 -0700

    initial commit

---
+++ crypt.py
@@ -0,0 +1,6 @@
+def main():
+    print(f'Inside crypt.py')
+
+
+if __name__ == '__main__':
+    main()
---
+++ genkeys.py
@@ -0,0 +1,6 @@
+def main():
+    print(f'Inside genkeys.py')
+
+
+if __name__ == '__main__':
+    main()
